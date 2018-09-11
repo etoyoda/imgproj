85d41ddd4f98923258c3e76ef3de831811097796
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-09-11 09:15:02 +0000

    give right urls

--- list.sh
+++ list.sh
@@ -20,15 +20,35 @@ do
        'puts Time.parse(ARGV.first).utc.strftime("%Y-%m-%dT%H%M")' ${dir})
     fi
     nm=X
-    url='https://www.jma.go.jp/jp/gms/large.html?area=6'
     case $dir in
-    gmsir*)  nm=Himawari-IR ;;
-    gmswv*)  nm=Himawari-WV ;;
-    gmsvs*)  nm=Himawari-VS ;;
-    gskwx*)  nm=SuikeiWx ;;
-    gskte*)  nm=SuikeiT ;;
-    gb??t*)  nm=T-fcst$(echo $dir | cut -b3-4) ;;
-    gb??w*)  nm=Wx-fcst$(echo $dir | cut -b3-4) ;;
+    gmsir*)
+      nm=Himawari-IR
+      url='https://www.jma.go.jp/jp/gms/large.html?area=6&element=0'
+      ;;
+    gmswv*)
+      nm=Himawari-WV
+      url='https://www.jma.go.jp/jp/gms/large.html?area=6&element=2'
+      ;;
+    gmsvs*)
+      nm=Himawari-VS
+      url='https://www.jma.go.jp/jp/gms/largec.html?area=6&element=1'
+      ;;
+    gskwx*)
+      nm=Suikei-Wx
+      url='http://www.data.jma.go.jp/obj/bunpu/index.html?000?wthr'
+      ;;
+    gskte*)
+      nm=Suikei-T
+      url='http://www.data.jma.go.jp/obj/bunpu/index.html?000?temp'
+      ;;
+    gb??t*)
+      nm=T-fcst$(echo $dir | cut -b3-4)
+      url='http://www.jma.go.jp/jp/mesh20/000.html?elementCode=1'
+      ;;
+    gb??w*)
+      nm=Wx-fcst$(echo $dir | cut -b3-4)
+      url='http://www.jma.go.jp/jp/mesh20/000.html?elementCode=0'
+      ;;
     esac
     echo "${dhn};\"${dir}\": {\"vn\":\"${nm}/${dhn}\", \"url\":\"${url}\"}," >> z.tmp
   else
