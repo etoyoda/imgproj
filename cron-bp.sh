7a9ea2be05b33a67667da4bc1bd88c776aa7ccbe
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-09-11 03:17:18 +0000

    tenki bunpu yohou

--- cron-bp.sh
+++ cron-bp.sh
@@ -4,6 +4,8 @@ PATH=/bin:/usr/bin:/usr/local/bin
 
 PUBDATA=/var/www/html/wisdata/arch
 
+set -e
+
 cd $(dirname $0)
 
 set $(TZ=JST-9 date '+%Y %m %d %H')
@@ -11,25 +13,33 @@ y=$1
 m=$2
 d=$3
 h=$4
-if (( h >= 18 )); then
+case "$h" in
+18|19|20|21|22|23)
   h=18
-  ff='04 07'
-elif (( h >= 6 )); then
+  ;;
+0[6-9]|1[0-7])
   h=06
-  ff='03 08'
-else
+  ;;
+*)
   set $(TZ=UTC date '+%Y %m %d')
   y=$1
   m=$2
   d=$3
   h=18
-  ff='04 07'
-fi
+  ;;
+esac
 ymdhn=${y}${m}${d}${h}00
 
-for f in $ff
+for item in cond temp
 do
-  for item in temp cond
+  ff=''
+  case "$item.$h" in
+  temp.06) ff='03 08' ;;  # 15JST, 1d+06JST
+  cond.06) ff='01 05 07' ;;  # 00Z, 12Z, 18Z
+  temp.18) ff='04 07' ;;  # 1d+06JST, 1d+15JST
+  cond.18) ff='01 05 08' ;;  # 
+  esac
+  for f in $ff
   do
     gbffx=dummy
     case $item in
@@ -40,16 +50,22 @@ do
       echo ${gbffx}.$ymdhn already present
     else
       mkdir -p ${gbffx}.$ymdhn
-      wget -q -O${gbffx}.${ymdhn}/${item}_000_${ymdhn}-${f}.png \
-        http://www.jma.go.jp/jp/mesh20/imgs/${item}/000/${ymdhn}-${f}.png
+      ruby -e 'y,m,d,h,f = ARGV.map{|s|s.to_i(10)};
+        puts((Time.gm(y,m,d,h)+(f*3-9)*3600).strftime("%Y %m %d %H %M"))' \
+        $y $m $d $h $f > ${gbffx}.$ymdhn/validtime.txt
+      url=http://www.jma.go.jp/jp/mesh20/imgs/${item}/000/${ymdhn}-${f}.png
+      if ! wget -q -O${gbffx}.${ymdhn}/${item}_000_${ymdhn}-${f}.png $url ; then
+        echo cannot download $url >&2
+        false
+      fi
       mkdir -p ${gbffx}.$ymdhn/4/{13,14}
-      ./imgproj -pr,ou,la118.7252,lz150.4015,ba47.2639,bz21.3982 \
+      ./imgproj -pr,ou,la118.7252,lz155.2842,ba47.2639,bz23.7904 \
         ${gbffx}.${ymdhn}/${item}_000_${ymdhn}-${f}.png \
-        ${gbffx}.$ymdhn/4/{13,14}/{5,6,7}.png
+        ${gbffx}.$ymdhn/4/{13,14}/{5,6}.png
       if test -d ${PUBDATA} ; then
         zip -qqr ${gbffx}.$ymdhn.zip ${gbffx}.$ymdhn
-        test -d ${PUBDATA}/files/$ym || mkdir ${PUBDATA}/files/$ym
-        mv ${gbffx}.$ymdhn.zip ${PUBDATA}/files/$ym
+        test -d ${PUBDATA}/files/${y}${m} || mkdir ${PUBDATA}/files/${y}${m}
+        mv ${gbffx}.$ymdhn.zip ${PUBDATA}/files/${y}${m}
       fi
     fi
 
--- list.sh
+++ list.sh
@@ -6,12 +6,19 @@ cd $(dirname $0)
 
 export TZ=JST-9
 
-echo '{' > z.tmp
+: > z.tmp
 for dir in g*.20*
 do
   if $(ruby -rtime -e 'exit(Time.parse(ARGV.first) > Time.now - 86400 ? 0 : 1)' $dir)
   then
-    dhn=$(ruby -rtime -e 'puts Time.parse(ARGV.first).utc.strftime("%Y-%m-%dT%H%M")' ${dir})
+    dhn=
+    if test -f $dir/validtime.txt ; then
+      set $(cat $dir/validtime.txt)
+      dhn=${1}-${2}-${3}T${4}${5}
+    else
+      dhn=$(ruby -rtime -e \
+       'puts Time.parse(ARGV.first).utc.strftime("%Y-%m-%dT%H%M")' ${dir})
+    fi
     nm=X
     url='https://www.jma.go.jp/jp/gms/large.html?area=6'
     case $dir in
@@ -20,13 +27,17 @@ do
     gmsvs*)  nm=Himawari-VS ;;
     gskwx*)  nm=SuikeiWx ;;
     gskte*)  nm=SuikeiT ;;
-    gb*) nm=Bunpu$(echo $dir | cut -b3-5 ) ;;
+    gb??t*)  nm=T-fcst$(echo $dir | cut -b3-4) ;;
+    gb??w*)  nm=Wx-fcst$(echo $dir | cut -b3-4) ;;
     esac
-    echo "\"${dir}\": {\"vn\":\"${nm}/${dhn}\", \"url\":\"${url}\"}," >> z.tmp
+    echo "${dhn};\"${dir}\": {\"vn\":\"${nm}/${dhn}\", \"url\":\"${url}\"}," >> z.tmp
   else
     rm -rf $dir
   fi
 done
-echo '"stop": "dummy" }' >> z.tmp
+echo '{' > z2.tmp
+sort z.tmp | sed 's/^[^;]*;//' >> z2.tmp
+echo '"stop": "dummy" }' >> z2.tmp
+rm -f z.tmp
 
-mv -f z.tmp tiles.json
+mv -f z2.tmp tiles.json
